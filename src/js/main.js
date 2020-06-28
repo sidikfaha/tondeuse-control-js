19e11365f513975f7037f375217d5d19dd662ba5
Joe <joefaha1@gmail.com>
2020-06-28 05:49:39 +0100

    Mise à jour des commentaires et du README

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Id des differentes orientations possibles
+ * Id des différentes orientations possibles
  *
  * @type {{S: number, E: number, W: number, N: number}}
  */
@@ -13,7 +13,7 @@ const Orientations = {
 }
 
 /**
- * Id des differentes directions possibles
+ * Id des différentes directions possibles
  *
  * @type {{D: number, G: number}}
  */
@@ -24,7 +24,12 @@ const Directions = {
 
 }
 
+/**
+ * Prefixe de la console
+ * @type {string}
+ */
 const C_PREFIX = `(console) >> `
+
 
 class Pelouse {
 
@@ -41,11 +46,11 @@ class Tondeuse {
 
 
     /**
-     * Creer une nouvelle tondeuse
+     * Créer une nouvelle tondeuse
      * @param {number} x
      * @param {number} y
      * @param {string} orientation - [N,E,S,W]
-     * @param {string} cmd - La commande a executer
+     * @param {string} cmd - La commande à exécuter
      * @param {Pelouse} pelouse - La pelouse sur laquelle elle va travailler
      */
     constructor(x, y, orientation, cmd, pelouse) {
@@ -58,7 +63,7 @@ class Tondeuse {
     }
 
     /**
-     * Changer de directixon
+     * Changer de direction
      *
      * @param {string} direction - Le sens de rotation [D => Droite, G => Dauche]
      */
@@ -67,15 +72,15 @@ class Tondeuse {
         const _o = this.orientationId
 
 
-        if (_o === 4 && _direction === 1) {
+        if (_o === 4 && _direction === 1) { // Orienter vers N si nous somme à W et que la direction est D
 
             this.orientationId = 1
 
-        } else if (_o === 1 && _direction === -1) {
+        } else if (_o === 1 && _direction === -1) { // Orienter vers W si nous somme à N et que la direction est G
 
             this.orientationId = 4
 
-        } else {
+        } else { // On passe à l'orientation suivante/precedente si tout est normal
 
             if (_direction === 1) {
                 this.orientationId++
@@ -91,8 +96,8 @@ class Tondeuse {
      * Deplacer la tondeuse dans le sens de sa direction actuelle sans oublier de ne pas deborder la cloture
      */
     move() {
-        const pelouseX = this.pelouse.x
-        const pelouseY = this.pelouse.y
+        const pelouseX = this.pelouse.x // La limite en abcisse
+        const pelouseY = this.pelouse.y // La limite en ordonnée
 
 
         switch (this.orientationId) {
@@ -113,15 +118,15 @@ class Tondeuse {
     }
 
     /**
-     * Recuperer les coordonnees acutuelles de la tondeuse et son orientation
+     * Recuperer les coordonnées acutuelles de la tondeuse et son orientation
      * @returns {string}
      */
     getState() {
-        return `${this.x} ${this.y} ${getOrientationTxt(this.orientationId)}`
+        return `${this.x} ${this.y} ${getOrientationTxt(this.orientationId)}` // Format de coordonnées x y orientation
     }
 
     /**
-     * Executer la serie de commandes permettant de controller le tracteur
+     * Exécuter la serie de commandes permettant de controller la tondeuse
      */
     run() {
         const _script = this.cmd.split('')
@@ -130,10 +135,10 @@ class Tondeuse {
             switch (s) {
                 case "D":
                 case "G":
-                    this.rorate(s)
+                    this.rorate(s) // Faire roter si c'est une requête de changement d'orientation
                     break
                 case "A":
-                    this.move()
+                    this.move() // Déplacer dans l'autre cas
                     break
             }
         })
@@ -141,6 +146,10 @@ class Tondeuse {
 
 }
 
+
+/**
+ * Cette classe se chargera d'interpréter le script et d'afficher les valeurs de retour dans la console
+ */
 class Runner {
 
     constructor() {
@@ -151,16 +160,15 @@ class Runner {
     }
 
     init() {
-        const lines = this.script.split(/\r?\n/)
-        const pelouseCoord = lines.shift().split(" ")
+        const lines = this.script.split(/\r?\n/) // Recuperation des commandes ligne par ligne (dans un tableau)
+        const pelouseCoord = lines.shift().split(" ") // Recuperation (sous forme de tableau) de la premiere ligne dediée à la taille de la pelouse et suppression du tableau
         const tondeusesDatas = lines
-        let listeTondeuses = []
+        let listeTondeuses = [] // TODO: Remplir la liste de tondeuses plus tard en fonctions des lignes du script
 
-        let pelouse = new Pelouse(pelouseCoord[0], pelouseCoord[1])
+        let pelouse = new Pelouse(pelouseCoord[0], pelouseCoord[1]) // Création de la pelouse
 
         for (let i = 0; i < tondeusesDatas.length; i += 2) {
 
-            console.log(tondeusesDatas)
             const tondCoord = tondeusesDatas[i].split(" ")
             const cmd = tondeusesDatas[i + 1]
 
@@ -172,21 +180,21 @@ class Runner {
     }
 
     exec() {
-        const tond = this.tondeuses
+        const tond = this.tondeuses // Raccourci
         let count = 1
 
         this.console.innerHTML = ""
-        this.w("Execution du script...")
-        this.w(`${tond.length} ${tond.length > 1 ? "tondeuses detectees" : "tondeuse detectee"}`)
+        this.w("Exécution du script...")
+        this.w(`${tond.length} ${tond.length > 1 ? "tondeuses détectées" : "tondeuse détectée"}`)
 
         tond.forEach(t => {
-            this.w(`Demarrage de la tondeuse N° ${count}...`)
+            this.w(`Démarrage de la tondeuse N° ${count}...`)
             t.run()
             this.w(`<span class="success">Position finale : ${t.getState()}</span>`)
             count++
         })
 
-        this.w("Execution terminee avec succes")
+        this.w(`<span class="succes">Exécution terminée avec succes</span>`)
 
     }
 
@@ -195,6 +203,11 @@ class Runner {
     }
 }
 
+/**
+ * Obtenir l'orientation en fonction de son identifiant
+ * @param {number} id
+ * @returns {string}
+ */
 function getOrientationTxt(id) {
     return ["N", "E", "S", "W"][id - 1]
 }
